cdd150ea60f7d5ade6d0432757b7008c7d4e213a
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2024-12-10 17:51:41 +0100

    Add udp networking working poc

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,17 @@
-#include "Network.hpp"
+#include <iostream>
+#include "UdpSocket.hpp"
 
-int main() {
-  Network k;
-  k.printHello();
+int main()
+{
+    UdpSocket serv;
+
+    serv.bindSocket("127.0.0.1", 12345);
+    Packet packet("127.0.0.1", 12345, 0, Packet::Type::GAME_START, 0, nullptr);
+    serv.sendTo(packet);
+    std::vector<Packet> packets = serv.receive();
+    for (const auto &i : packets) {
+        Packet::Type type = i.getType();
+        const char *to_str = type == Packet::Type::GAME_START ? "GAME_START" : "false";
+        std::cout << i.getSenderIp() << ":" << to_str << std::endl;
+    }
 }
