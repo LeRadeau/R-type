62bacd1e550dd268c97e26fe82c31653f3daf868
Matthias SOUAL <matthias.soual@epitech.eu>
2024-12-17 18:24:01 +0100

    Fix server now use serializer

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <unordered_map>
-#include <cstdint>
+#include "./../common/Serializer.hpp"
+#include "./../common/network_types.hpp"
 
 #define SERVER_PORT 54000
 
@@ -30,38 +31,40 @@ int main() {
     std::cout << "Server is running on port " << SERVER_PORT << std::endl;
 
     while (true) {
-        sf::Packet packet;
-        if (socket.receive(packet, sender, senderPort) == sf::Socket::Done) {
-            std::string messageType;
-            packet >> messageType;
+        char data[1024];
+        std::size_t received;
 
-            if (messageType == "connect") {
-                std::string newUsername;
-                packet >> newUsername;
+        if (socket.receive(data, sizeof(data), received, sender, senderPort) == sf::Socket::Done) {
+            const char* ptr = data;
+            auto messageType = static_cast<MessageType>(Serializer::deserialize<uint8_t>(ptr));
+
+            if (messageType == MessageType::CONNECT) {
+                std::string newUsername = Serializer::deserializeString(ptr);
 
                 if (clients.find(newUsername) != clients.end()) {
                     std::cout << "Client already exists: " << newUsername << std::endl;
-                    sf::Packet errorPacket;
-                    errorPacket << "error" << "Username already exists";
-                    socket.send(errorPacket, sender, senderPort);
+
+                    std::string errorBuffer;
+                    Serializer::serialize(errorBuffer, static_cast<uint8_t>(MessageType::ERROR));
+                    Serializer::serialize(errorBuffer, "Username already exists");
+                    socket.send(errorBuffer.data(), errorBuffer.size(), sender, senderPort);
                     continue;
-                } else {
-                    clients[newUsername] = Client(sender, senderPort, newUsername);
-                    std::cout << "New client connected: " << newUsername << std::endl;
                 }
 
+                clients[newUsername] = Client(sender, senderPort, newUsername);
+                std::cout << "New client connected: " << newUsername << std::endl;
 
-            } else if (messageType == "move") {
-                std::string movingUsername;
-                float x, y;
-                packet >> movingUsername >> x >> y;
+            } else if (messageType == MessageType::MOVE) {
+                std::string movingUsername = Serializer::deserializeString(ptr);
+                float x = Serializer::deserialize<float>(ptr);
+                float y = Serializer::deserialize<float>(ptr);
 
                 if (clients.find(movingUsername) != clients.end()) {
                     clients[movingUsername].position = {x, y};
                 }
-            } else if (messageType == "goodbye") {
-                std::string leavingUsername;
-                packet >> leavingUsername;
+
+            } else if (messageType == MessageType::GOODBYE) {
+                std::string leavingUsername = Serializer::deserializeString(ptr);
 
                 if (clients.find(leavingUsername) != clients.end()) {
                     clients.erase(leavingUsername);
@@ -69,17 +72,19 @@ int main() {
                 }
             }
 
-            // Broadcast all client positions
-            sf::Packet updatePacket;
-            updatePacket << "update_clients";
-            updatePacket << static_cast<uint32_t>(clients.size());
+            // Broadcast updated client positions
+            std::string updateBuffer;
+            Serializer::serialize(updateBuffer, static_cast<uint8_t>(MessageType::UPDATE_CLIENTS));
+            Serializer::serialize(updateBuffer, static_cast<uint32_t>(clients.size()));
 
             for (const auto& [username, client] : clients) {
-                updatePacket << client.username << client.position.x << client.position.y;
+                Serializer::serialize(updateBuffer, client.username);
+                Serializer::serialize(updateBuffer, client.position.x);
+                Serializer::serialize(updateBuffer, client.position.y);
             }
 
             for (const auto& [username, client] : clients) {
-                socket.send(updatePacket, client.ip, client.port);
+                socket.send(updateBuffer.data(), updateBuffer.size(), client.ip, client.port);
             }
         }
     }
