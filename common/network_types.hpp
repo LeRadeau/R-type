eb9da263e8af06d5ccb91485eadcc8761838f1aa
Matthias SOUAL <matthias.soual@epitech.eu>
2025-01-07 11:18:25 +0100

    Add Ennemies and bullets updates packet types

--- common/network_types.hpp
+++ common/network_types.hpp
@@ -3,9 +3,16 @@
 #include <cstdint>
 
 enum class MessageType : uint8_t {
-    CONNECT = 0,
-    GOODBYE = 1,
-    MOVE = 2,
-    UPDATE_CLIENTS = 3,
-    ERROR = 4
-};
+        // CLIENT -> SERVER
+        CONNECT = 0,
+        GOODBYE = 1,
+        MOVE = 2,
+        SHOOT = 5,
+        // SERVER -> CLIENT
+        UPDATE_CLIENTS = 3,
+        UPDATE_BULLETS = 6,
+        UPDATE_ENEMIES = 7,
+        DESTROY_BULLET = 8,
+        // ERROR
+        ERROR = 4
+    };
