12b1942ba52d0cba3a31011b9d636cfa677beee9
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2025-01-07 21:58:41 +0100

    Move NetworkManager method definitions in a cpp file

--- client/ecs/NetworkManager.hpp
+++ client/ecs/NetworkManager.hpp
@@ -1,54 +1,22 @@
 #pragma once
 #include <SFML/Network.hpp>
-#include <thread>
 #include <mutex>
 #include <queue>
-#include <unordered_map>
 #include <string>
-#include <iostream>
+#include <thread>
 
 class NetworkManager {
-public:
-    NetworkManager(const std::string& serverIp, uint16_t port) : 
-        serverIp(serverIp), serverPort(port) {
-        if (socket.bind(sf::Socket::AnyPort) != sf::Socket::Done) {
-            throw std::runtime_error("Failed to bind socket");
-        }
-        receiverThread = std::thread(&NetworkManager::receiveMessages, this);
-    }
+  public:
+    NetworkManager(const std::string &serverIp, uint16_t port);
 
-    ~NetworkManager() {
-        if (receiverThread.joinable()) {
-            receiverThread.join();
-        }
-    }
+    ~NetworkManager();
 
-    void send(const std::string& buffer) {
-        socket.send(buffer.data(), buffer.size(), serverIp, serverPort);
-    }
+    void send(const std::string &buffer);
 
-    std::queue<std::string> getReceivedMessages() {
-        std::lock_guard<std::mutex> lock(queueMutex);
-        return receivedMessages;
-    }
+    std::queue<std::string> &getReceivedMessages();
 
-private:
-    void receiveMessages() {
-        char data[1024];
-        std::size_t received;
-        sf::IpAddress sender;
-        unsigned short senderPort;
-
-        while (true) {
-            if (socket.receive(data, sizeof(data), received, sender, senderPort) == sf::Socket::Done) {
-                std::lock_guard<std::mutex> lock(queueMutex);
-                receivedMessages.push(std::string(data, received));
-                if (receivedMessages.size() > 10) {
-                    receivedMessages = std::queue<std::string>();
-                }
-            }
-        }
-    }
+  private:
+    void receiveMessages();
 
     sf::UdpSocket socket;
     sf::IpAddress serverIp;
