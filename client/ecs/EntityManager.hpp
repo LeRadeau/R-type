74ff842ae22b220f9bc57d3a5927d72e1c6e5227
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2025-01-07 22:46:25 +0100

    Move EntityManager methods definition to a cpp file

--- client/ecs/EntityManager.hpp
+++ client/ecs/EntityManager.hpp
@@ -4,28 +4,10 @@
 
 class EntityManager {
   public:
-    Entity &createEntity()
-    {
-        entities.push_back(std::make_unique<Entity>());
-        return *entities.back();
-    }
+    Entity &createEntity();
+    void markForDeletion(uint64_t id);
+    void destroyMarkedEntities();
 
-    void markForDeletion(uint64_t id)
-    {
-        toBeDeleted.push_back(id);
-    }
-
-    void destroyMarkedEntities()
-    {
-        for (uint64_t id : toBeDeleted) {
-            entities.erase(std::remove_if(entities.begin(), entities.end(),
-                               [id](const auto &e) {
-                                   return e->getId() == id;
-                               }),
-                entities.end());
-        }
-        toBeDeleted.clear();
-    }
     std::vector<std::unique_ptr<Entity>> entities;
 
   private:
