a86856277c4d1aa7f8d3d5a0ee28b3b8da3802f3
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2025-01-08 20:30:37 +0100

    Remove duplicate component

--- client/ecs/system/MovementSystem.cpp
+++ client/ecs/system/MovementSystem.cpp
@@ -2,8 +2,8 @@
 #include "Serializer.hpp"
 #include "ecs/component/BulletIdComponent.hpp"
 #include "ecs/component/InputComponent.hpp"
-#include "ecs/component/NetworkComponent.hpp"
 #include "ecs/component/PositionComponent.hpp"
+#include "ecs/component/UsernameComponent.hpp"
 #include "ecs/component/VelocityComponent.hpp"
 #include "network_types.hpp"
 
@@ -18,12 +18,12 @@ void MovementSystem::update(EntityManager &entityManager, NetworkManager &networ
     for (auto &entity : entityManager.entities) {
         auto *input = entity->getComponent<InputComponent>();
         auto *position = entity->getComponent<PositionComponent>();
-        auto *network = entity->getComponent<NetworkComponent>();
+        auto *username = entity->getComponent<usernameComponent>();
         auto *velocity = entity->getComponent<VelocityComponent>();
         auto *bulletId = entity->getComponent<BulletIdComponent>();
 
         // Movement logic for players
-        if (input && position && network && (input->moveLeft || input->moveRight || input->moveUp || input->moveDown)
+        if (input && position && username && (input->moveLeft || input->moveRight || input->moveUp || input->moveDown)
             && focus) {
             if (input->moveLeft)
                 position->position.x -= 200.0f * deltaTime;
@@ -38,7 +38,7 @@ void MovementSystem::update(EntityManager &entityManager, NetworkManager &networ
             if (timer > 0.1f) {
                 std::string buffer;
                 Serializer::serialize(buffer, static_cast<uint8_t>(MessageType::MOVE));
-                Serializer::serialize(buffer, network->username);
+                Serializer::serialize(buffer, username->username);
                 Serializer::serialize(buffer, position->position.x);
                 Serializer::serialize(buffer, position->position.y);
                 networkManager.send(buffer);
@@ -50,7 +50,7 @@ void MovementSystem::update(EntityManager &entityManager, NetworkManager &networ
         if (input && position && input->spaceBar && bulletTimer > 0.3f && focus) {
             std::string buffer;
             Serializer::serialize(buffer, static_cast<uint8_t>(MessageType::SHOOT));
-            Serializer::serialize(buffer, network->username);
+            Serializer::serialize(buffer, username->username);
             Serializer::serialize(buffer, position->position.x);
             Serializer::serialize(buffer, position->position.y);
             networkManager.send(buffer);
