5155ae7cc79018838b32c3160e751542b2386901
xBilalx <bilal.ihallaine@epitech.eu>
2025-01-13 17:20:01 +0100

    Add movement Sinusoidale for the mob

--- server/game_state.cpp
+++ server/game_state.cpp
@@ -1,4 +1,6 @@
 #include "Server.hpp"
+#include <math.h> 
+#include <iostream>
 
 void Server::updateBullets(float deltaTime)
 {
@@ -18,8 +20,10 @@ void Server::updateEnnemies(float deltaTime)
 {
     for (auto &ennemy : ennemies_) {
         if (ennemy.isAlive) {
-            ennemy.position.x += ennemy.velocity.x * deltaTime;
-            ennemy.position.y += ennemy.velocity.y * deltaTime;
+            ennemy.position.x += (ennemy.velocity.x) * deltaTime;
+
+            ennemy.position.y = ennemy.startingY + ennemy.amplitude * std::sin(ennemy.cosinus * ennemy.frequency);
+            ennemy.cosinus += deltaTime;
         } else {
             ennemy.respawnCooldown -= deltaTime;
             if (ennemy.respawnCooldown <= 0) {
@@ -36,11 +40,15 @@ void Server::loadEnnemies()
         Ennemy newEnnemy;
         newEnnemy.id = "ennemy_" + std::to_string(i);
         newEnnemy.position = {float(1000), float(100 * i)};
-        newEnnemy.velocity = {0, 0};
+        newEnnemy.velocity = {50, 15};
         newEnnemy.health = 100;
         newEnnemy.shootingCooldown = 1.0f;
         newEnnemy.respawnCooldown = 5.0f;
         newEnnemy.isAlive = true;
+        newEnnemy.amplitude = 70.0f;
+        newEnnemy.frequency = 1.0f;
+        newEnnemy.cosinus = 1;
+        newEnnemy.startingY = newEnnemy.position.y;
         ennemies_.push_back(newEnnemy);
     }
 }
