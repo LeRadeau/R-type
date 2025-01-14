ac4360e915ea34462b0e6fc5149bb975dc71c573
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2025-01-14 01:00:18 +0100

    Add player and bydos pixel art

--- client/ecs/entity/PlayerEntity.cpp
+++ client/ecs/entity/PlayerEntity.cpp
@@ -2,14 +2,14 @@
 #include "ecs/NetworkManager.hpp"
 #include "ecs/component/InputComponent.hpp"
 #include "ecs/component/PositionComponent.hpp"
-#include "ecs/component/RenderComponent.hpp"
+#include "ecs/component/SpriteComponent.hpp"
 #include "ecs/component/UsernameComponent.hpp"
 
 PlayerEntity::PlayerEntity(EntityManager &entityManager, const std::string &username, NetworkManager &networkManager)
     : entityManager_(entityManager), entity_(entityManager.createEntity())
 {
     entity_.addComponent<PositionComponent>(400, 300);
-    entity_.addComponent<RenderComponent>(30, sf::Color::Green);
+    entity_.addComponent<SpriteComponent>("assets/Player64x64.png", sf::Vector2f(1, 1));
     entity_.addComponent<InputComponent>();
     entity_.addComponent<usernameComponent>(username);
     networkManager.send(MessageType::CONNECT, username);
