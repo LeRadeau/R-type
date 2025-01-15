7ca06857f2558d964ebe70e8c1a6bebaf66e89f9
MuMyungMyung <180272828+MuMyungMyung@users.noreply.github.com>
2025-01-16 00:33:43 +0100

    Remove unused function

--- client/ecs/system/MessageSystem.hpp
+++ client/ecs/system/MessageSystem.hpp
@@ -13,7 +13,6 @@ class MessageSystem {
   private:
     void handleLaunchGame(
         EntityManager &entityManager, std::unique_ptr<PlayerEntity> &player, const std::string &username);
-    void handleWaitLobby(const char *&ptr);
     void handleUpdateClients(EntityManager &entityManager, const char *&ptr, const std::string &localUsername);
     void handleUpdateBullets(EntityManager &entityManager, const char *&ptr);
     void handleError(const char *&ptr);
